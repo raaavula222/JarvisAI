cc5c45bdbfd908003d4cc02c8481ed69aeccdd14
HARXHIT9 <hvats0708@gmail.com>
2024-07-24 20:36:17 +0530

    Changed the voice ID to Microsoft David in the main Jarvis.py file

--- Jarvis.py
+++ Jarvis.py
@@ -1,5 +1,7 @@
 import pyttsx3
 engine = pyttsx3.init() #Takes reference from pyttsc3
+voice = engine.getProperty('voices')
+engine.setProperty('voice', voice[1].id)
 text = input('Enter Your Text \n')
 engine.say(text)
 engine.runAndWait()
