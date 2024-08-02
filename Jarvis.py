2c91bac5b578d3747843866942c0f8e90295753f
breeze-sn <nagekarsimran@gmail.com>
2024-08-02 09:16:13 +0530

    Created a speech recognizing function

--- Jarvis.py
+++ Jarvis.py
@@ -1,7 +1,21 @@
 import pyttsx3
-engine = pyttsx3.init() #Takes reference from pyttsc3
-voice = engine.getProperty('voices')
-engine.setProperty('voice', voice[1].id)
-text = input('Enter Your Text \n')
-engine.say(text)
-engine.runAndWait()
+import speech_recognition as sr
+
+# Speak Engine
+# engine = pyttsx3.init() #Takes reference from pyttsc3
+# voice = engine.getProperty('voices')
+# engine.setProperty('voice', voice[1].id)
+# engine.say(text)
+# engine.runAndWait()
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
